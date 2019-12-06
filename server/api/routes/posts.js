4f2950b5b23466ed55f3a54b31f32bdab4e7068c
Ruben COETZER <rcoetzer@c4r21s4.wethinkcode.co.za>
2019-12-06 09:22:58 +0200

    first steps

---
+++ server/api/routes/posts.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mongodb = require('mongodb');
+const router = express.Router();
+
+
+const dbstring = process.env.DBSTING || 'mongodb+srv://Ruben:lXs4sIXATIdXdemr@home-cp0vu.mongodb.net/test?retryWrites=true&w=majority';
+const dbname = process.env.DBNAME || 'matcha';
+
+// Get Posts
+router.get('/', async (req, res) => {
+	const posts = await loadpostCollection();
+	res.send(await posts.find({}).toArray());
+});
+
+// Add Posts
+router.post('/', async (req, res) => {
+	const posts = await loadpostCollection();
+	await posts.insertOne({
+		text: req.body.text,
+		createdAt: new Date()
+		});
+		res.status(201).send();
+	});
+// Delete posts
+
+router.delete('/:PostId', async (req, res) => {
+		const posts = await loadpostCollection();
+		await posts.deleteOne({_id: new mongodb.ObjectID(req.params.PostId)});
+		res.status(200).send();
+})
+async function loadpostCollection() {
+	const client = await mongodb.MongoClient.connect(dbstring, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true 
+	});
+
+	return client.db(dbname).collection('posts');
+}
+module.exports = router;
---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+//Middleware
+app.use(bodyparser.json());
+app.use(cors());
+
+const posts = require('./api/routes/posts');
+app.use('/api/posts', posts);
+
+app.listen(port, ()=> console.log(`server started on port ${port}`));
